9b5e396da8a25bf0184023461bce70564f50df7e
MS06J <iamavery5417@gmail.com>
2023-07-31 15:04:46 +0200

    Final Presentation

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import quaternion
-
-a=np.array(['1','2','3','hahah'])
-for i in range(len(a)):
-        a[i]=int(a[i])
-print(a)
--- test_blender.py
+++
@@ -1,7 +0,0 @@
-import bpy
-armature_objects = [obj for obj in bpy.context.scene.objects if obj.type == 'ARMATURE']
-for armature_obj in armature_objects:
-    if armature_obj.name == 'metarig':
-        for starting_bone in armature_obj.data.bones:
-            print(starting_bone.children[0].name, starting_bone.children[0].tail_local)
-            break
